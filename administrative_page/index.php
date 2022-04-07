50baef2268d2d6c8ea470454c7c53e88bbf7971e
Han Cho <freetyhan@gmail.com>
2022-04-07 13:52:48 -0700

    title updated

--- administrative_page/index.php
+++ administrative_page/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>administrative_page_CPEN391</title>
+    <title>Twitter Slideshow - Admin</title>
     <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
     <link rel="stylesheet" href="./assets/css/Lightbox-Gallery.css">
